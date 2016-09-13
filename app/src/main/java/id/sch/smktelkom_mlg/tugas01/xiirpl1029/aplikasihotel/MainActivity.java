27edb1fe6e06448b2227a8f291b636bd4414afba
Nabila <nabila_nugraheni_23rpl@student.smktelkom-mlg.sch.id>
2016-09-13 20:44:16 +0700

    + 1. Menambahkan method doclick untukmenampilkan hasil sat diklik button 2. menambahkan method isValid untuk cek kesalahan
    -
    1. Mengurangi deklarasi radiobutton karena jadi satu di radiogroup

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1029/aplikasihotel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1029/aplikasihotel/MainActivity.java
@@ -25,7 +25,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvHasil,tvHasil2, tvTanggal, tvFasilitas;
+    TextView tvHasil, tvTanggal, tvFasilitas;
     int nFasilitas;
     Button buttonPesan;
     Spinner sptipekmr, spSubkm;
@@ -69,13 +69,126 @@ public class MainActivity extends AppCompatActivity {
         cbGym = (CheckBox) findViewById(R.id.checkBoxGym);
         cb24 = (CheckBox) findViewById(R.id.checkBox24);
 
-        rbTunai = (RadioButton) findViewById(R.id.radioButtonTunai);
-        rbKartu = (RadioButton) findViewById(R.id.radioButtonKredit);
-        rbVoucher = (RadioButton) findViewById(R.id.radioButtonVoucher);
+//        rbTunai = (RadioButton) findViewById(R.id.radioButtonTunai);
+//        rbKartu = (RadioButton) findViewById(R.id.radioButtonKredit);
+//        rbVoucher = (RadioButton) findViewById(R.id.radioButtonVoucher);
         rgBayar=(RadioGroup) findViewById(R.id.radiogroupBayar);
 
         buttonPesan = (Button) findViewById(R.id.buttonOK);
         tvFasilitas= (TextView) findViewById(R.id.textViewaFasilitas);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        sptipekmr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                listSubtipe.clear();
+                listSubtipe.addAll(Arrays.asList(arSub[pos]));
+                adapter.notifyDataSetChanged();
+                spSubkm.setSelection(0);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        buttonPesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
+        if (isValid()) {
+            String nama = editnama.getText().toString();
+
+            String noktp = editnoktp.getText().toString();
+            String notelp = editnotelp.getText().toString();
+
+            String hasil = null;
+            if(rgBayar.getCheckedRadioButtonId()!=-1){
+                RadioButton rb = (RadioButton)
+                        findViewById(rgBayar.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+
+            String hasil2="Fasilitas yang dipilih : \n";
+            int startlen = hasil2.length();
+            if (cbLunch.isChecked()) hasil2 +=cbLunch.getText() +"\n";
+            if (cbDinner.isChecked()) hasil2 +=cbDinner.getText() +"\n";
+            if(cbKaraoke.isChecked()) hasil2 +=cbKaraoke.getText() +"\n";
+            if (cbGym.isChecked()) hasil2 +=cbGym.getText() +"\n";
+            if (cbSpa.isChecked()) hasil2 +=cbSpa.getText() +"\n";
+            if (cb24.isChecked()) hasil2 +=cb24.getText() +"\n";
+
+            if (hasil2.length()==startlen) hasil2="Tidak memilih fasilitas tambahan";
+
+            tvHasil.setText("Nama Anda                 : " + nama + "\n\n" +
+                    "No KTP Anda               : " + noktp + "\n\n" +
+                    "No. Telepon Anda          : " + notelp + "\n\n" +
+                    "Anda Memilih Tipe Kamar   : " + sptipekmr.getSelectedItem().toString() + "dengan Jenis Kamar " + spSubkm.getSelectedItem().toString() +"\n\n"+
+                    hasil2 + "\n\n"+
+                    "Anda Melakukan Pembayaran : " + hasil + "\n\n"
+            );
+        }
+    }
+    private boolean isValid() {
+        boolean valid=true;
+
+        String nama = editnama.getText().toString();
+        String noktp = editnoktp.getText().toString();
+        String notelp = editnotelp.getText().toString();
+
+        if(nama.isEmpty())
+        {
+            editnama.setError("Nama Belum Diisir");
+            valid=false;
+        }
+
+        else if(nama.length()<2)
+        {
+            editnama.setError("Nama kurang dari 2 karakter");
+            valid=false;
+        }
+
+        else {
+            editnama.setError(null);
+        }
+
+        if(noktp.isEmpty())
+        {
+            editnoktp.setError("No. KTP Belum Diisi");
+            valid=false;
+        }
+
+        else if(noktp.length()!=16)
+        {
+            editnoktp.setError("No. KTP yang Anda Inputkan Tidak Tepat");
+            valid=false;
+        }
+
+        else {
+            editnoktp.setError(null);
+        }
+
+        if(notelp.isEmpty())
+        {
+            editnotelp.setError("No. Telepon Belum Diisi");
+            valid=false;
+        }
+
+        else if(notelp.length()<8)
+        {
+            editnotelp.setError("No. Telepon kurang dari 8 karakter");
+            valid=false;
+        }
+
+        else {
+            editnotelp.setError(null);
+        }
+        return valid;
     }
 }
